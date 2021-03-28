a92ba9920d3e715993307bf454b1d81ec70ec235
Antonio Martins <antonioc@construtordevendas.com.br>
2021-03-28 12:14:57 -0300

    (tests) Criação de factories e seeders

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -2,52 +2,46 @@
 
 namespace Database\Factories;
 
+use App\Constantes\TiposUsuariosConstante;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-use Faker\Generator as Faker;
+use Faker\Provider\pt_BR\Person;
 
-///**
-// * Class UserFactory
-// * @package Database\Factories
-// * @author Antonio Martins
-// */
-//class UserFactory extends Factory
-//{
-//    /**
-//     * The name of the factory's corresponding model.
-//     *
-//     * @var string
-//     */
-//    protected $model = User::class;
-//
-//    /**
-//     * Define the model's default state.
-//     *
-//     * @return array
-//     */
-//    public function definition()
-//    {
-//        return [
-//            'name' => $this->faker->name,
-//            'email' => $this->faker->unique()->safeEmail,
-//            'email_verified_at' => now(),
-//            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-//            'remember_token' => Str::random(10),
-//        ];
-//    }
-//
-//    /**
-//     * Indicate that the model's email address should be unverified.
-//     *
-//     * @return \Illuminate\Database\Eloquent\Factories\Factory
-//     */
-//    public function unverified()
-//    {
-//        return $this->state(function (array $attributes) {
-//            return [
-//                'email_verified_at' => null,
-//            ];
-//        });
-//    }
-//}
+/**
+ * Class UserFactory
+ * @package Database\Factories
+ * @author Antonio Martins
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $fakerBr = new Person($this->faker);
+
+
+        return [
+            'nome' => "{$this->faker->firstName} {$this->faker->lastName}",
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => $this->faker->password(),
+            'cpf_cnpj' => $fakerBr->cpf(false),
+            'tipo' => $this->faker->randomElement(
+                [
+                    TiposUsuariosConstante::USUARIO_COMUM,
+                    TiposUsuariosConstante::USUARIO_LOJISTA
+                ]
+            )
+        ];
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Carteira;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
+/**
+ * Class DatabaseSeeder
+ * @package Database\Seeders
+ * @author Antonio Martins
+ */
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +20,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $usuarios = User::factory(10)->create();
+        $usuarios->each(
+            fn (User $user) => Carteira::factory(
+                [
+                    'id_usuario' => $user->id,
+                    'saldo' => 100
+                ]
+            )->create()
+        );
     }
 }
